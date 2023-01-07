573d3d2c9995be8cd67f2c59a4112fe17998aa1a
ahfish <ahfish@gmail.com>
2023-01-07 22:04:50 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
 // import React from 'react';
 import React, { Component } from "react";
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Line from "./Line";
-import CandleStick from "./CandleStick";
+import series1 from './CandleStickData_1';
+import series2 from './CandleStickData_2';
+import series3 from './CandleStickData_3';
+import series4 from './CandleStickData_4';
+import { CandleStick, CandleStickProp } from "./CandleStick";
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
+import 'bootstrap/dist/css/bootstrap.min.css';  
 
 function App() {
+  const [value,setValue]=useState("Series1");
+  const handleSelect=(eventKey: any, event: Object)=>{
+    console.log(eventKey);
+    setValue(eventKey)
+  }
+
   return (
+    <div className="App">
+      <DropdownButton
+      title="Select Series"
+      id="dropdown-menu-align-right"
+      onSelect={handleSelect}
+        >
+              <Dropdown.Item eventKey="Series1">Series 1</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item eventKey="Series2">Series 2</Dropdown.Item>
+              <Dropdown.Item eventKey="Series3">Series 3</Dropdown.Item>
+              <Dropdown.Item eventKey="Series4">Series 4</Dropdown.Item>
+      </DropdownButton>
+      <header className="App-header">
+          { value == "Series1" && <CandleStick series={series1} title="Series1"></CandleStick> }
+          { value == "Series2" && <CandleStick series={series2} title="Series2"></CandleStick> }
+          { value == "Series3" && <CandleStick series={series3} title="Series3"></CandleStick> }
+          { value == "Series4" && <CandleStick series={series4} title="Series4"></CandleStick> }
+       </header>
+    </div>
     // <div className="App">
     //   <header className="App-header">
-      <CandleStick></CandleStick>
+      // <CandleStick series={series}></CandleStick>
     //   </header>
     // </div>
   );
--- src/CandleStick.tsx
+++ src/CandleStick.tsx
@@ -2,90 +2,96 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import ReactApexChart from 'react-apexcharts'
 import './CandleStick.css'
-import series from './CandleStickData_first_level';
+// import series from './CandleStickData_second_level';
+import series from './CandleStickData_2';
 
 
-const opts: ApexCharts.ApexOptions = {
-        plotOptions: {
-          bar: {
-            columnWidth: '20%',
-          }
-        },
-        chart: {
-            type: 'candlestick',
-            // width: 1000,
-            zoom: {
-              enabled: true,
-              type: 'xy'
-            }
-        },
-        title: {
-            text: 'CandleStick Chart',
-            align: 'left'
-        },
-        stroke: {
-            width: [3, 1]
-          },
-        xaxis: {
-            type: 'datetime',
-            // type: "numeric",
-            tickAmount: 'dataPoints',
-            labels: {
-                show: true,
-                style: {
-                  colors: 'white',
-                  fontSize: '12px'
-                }
-                // formatter: function (value) {
-                //   return "";
-              }
-        },
-        yaxis: {
-            tickAmount: 10,
-             max: 172,
-            tooltip: {
-                enabled: true
-            },
-            labels: {
-                show: true,
-                style: {
-                  colors: 'white',
-                  fontSize: '12px'
-                },
-              }            
-        },
-        tooltip: {
-            x: {
-              format: 'yyyy-MM-dd HH:mm:ss'
-            },
-            shared: true,
-            custom: [function({seriesIndex, dataPointIndex, w}) {
-              return w.globals.series[seriesIndex][dataPointIndex]
-            }, function({ seriesIndex, dataPointIndex, w }) {
-              var o = w.globals.seriesCandleO[seriesIndex][dataPointIndex]
-              var h = w.globals.seriesCandleH[seriesIndex][dataPointIndex]
-              var l = w.globals.seriesCandleL[seriesIndex][dataPointIndex]
-              var c = w.globals.seriesCandleC[seriesIndex][dataPointIndex]
-              return (
-                 "O:" + o + "<br/>" +
-                 "H:" + h + "<br/>" +
-                 "L:" + l + "<br/>" +
-                 "C:" + c + "<br/>"
-              )
-            }]
-          }
+
+
+export interface CandleStickProp {
+  series : ApexAxisChartSeries,
+  title : string
 }
 
 
+export class CandleStick extends React.Component<CandleStickProp> {
+  opts: ApexCharts.ApexOptions = {}
 
-
-
-class CandleStick extends React.Component {
-    constructor(props: any) {
+    constructor(props: CandleStickProp) {
         super(props);
-
         this.state = {
         };
+
+        this.opts = {
+          plotOptions: {
+            bar: {
+              columnWidth: '20%',
+            }
+          },
+          chart: {
+              type: 'candlestick',
+              // width: 1000,
+              zoom: {
+                enabled: true,
+                type: 'xy'
+              }
+          },
+          title: {
+              text: props.title,
+              align: 'left'
+          },
+          stroke: {
+              width: [3, 1]
+            },
+          xaxis: {
+              type: 'datetime',
+              // type: "numeric",
+              tickAmount: 'dataPoints',
+              labels: {
+                  show: true,
+                  style: {
+                    colors: 'white',
+                    fontSize: '12px'
+                  }
+                  // formatter: function (value) {
+                  //   return "";
+                }
+          },
+          yaxis: {
+              tickAmount: 10,
+               max: 172,
+              tooltip: {
+                  enabled: true
+              },
+              labels: {
+                  show: true,
+                  style: {
+                    colors: 'white',
+                    fontSize: '12px'
+                  },
+                }            
+          },
+          tooltip: {
+              x: {
+                format: 'yyyy-MM-dd HH:mm:ss'
+              },
+              shared: true,
+              custom: [function({seriesIndex, dataPointIndex, w}) {
+                return w.globals.series[seriesIndex][dataPointIndex]
+              }, function({ seriesIndex, dataPointIndex, w }) {
+                var o = w.globals.seriesCandleO[seriesIndex][dataPointIndex]
+                var h = w.globals.seriesCandleH[seriesIndex][dataPointIndex]
+                var l = w.globals.seriesCandleL[seriesIndex][dataPointIndex]
+                var c = w.globals.seriesCandleC[seriesIndex][dataPointIndex]
+                return (
+                   "O:" + o + "<br/>" +
+                   "H:" + h + "<br/>" +
+                   "L:" + l + "<br/>" +
+                   "C:" + c + "<br/>"
+                )
+              }]
+            }
+          }
     }
 
 
@@ -94,7 +100,7 @@ class CandleStick extends React.Component {
             <div className="CandleStick">
                 <header className="CandleStick-header">
                 <div id="chart">
-                    <ReactApexChart options={opts} series={series} height={820} width={1900} />
+                    <ReactApexChart options={this.opts} series={this.props.series} height={890} width={1900} title="Name" />
                 </div>
                 <div id="html-dist"></div>
                 </header>
@@ -103,4 +109,4 @@ class CandleStick extends React.Component {
     }
 }
 
-export default CandleStick;
+// export default CandleStick;
