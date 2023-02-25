022bacc8893e7f3a1a5e3d04d0aafc8e3115405f
ahfish <ahfish@gmail.com>
2023-02-25 09:04:46 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ import './App.css';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Stack from 'react-bootstrap/Stack';
+import FormLabel from 'react-bootstrap/FormLabel';
 import Button from 'react-bootstrap/Button';
 import FormControl from "react-bootstrap/FormControl";
 import DropDownSearch from './DropDownSearch';
+import DatePicker from 'react-datepicker';
 
 import Line from "./Line";
 // import series0 from './CandleStickData_0';
@@ -22,16 +24,16 @@ import Line from "./Line";
 // import series7 from './CandleStickData_7';
 // import series8 from './CandleStickData_8';
 // import series9 from './CandleStickData_9';
-import series_2level_0 from './CandleStickData_2level_0';
-import series_2level_1 from './CandleStickData_2level_1';
-import series_2level_2 from './CandleStickData_2level_2';
-import series_2level_3 from './CandleStickData_2level_3';
-import series_2level_4 from './CandleStickData_2level_4';
-import series_2level_5 from './CandleStickData_2level_5';
-import series_2level_6 from './CandleStickData_2level_6';
-import series_2level_7 from './CandleStickData_2level_7';
-import series_2level_8 from './CandleStickData_2level_8';
-import series_2level_9 from './CandleStickData_2level_9';
+// import series_2level_0 from './CandleStickData_2level_0';
+// import series_2level_1 from './CandleStickData_2level_1';
+// import series_2level_2 from './CandleStickData_2level_2';
+// import series_2level_3 from './CandleStickData_2level_3';
+// import series_2level_4 from './CandleStickData_2level_4';
+// import series_2level_5 from './CandleStickData_2level_5';
+// import series_2level_6 from './CandleStickData_2level_6';
+// import series_2level_7 from './CandleStickData_2level_7';
+// import series_2level_8 from './CandleStickData_2level_8';
+// import series_2level_9 from './CandleStickData_2level_9';
 import { CandleStick, CandleStickProp } from "./CandleStick";
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown'
@@ -49,31 +51,51 @@ function App() {
 
   return (
     <div className="App">
-      <Stack direction="horizontal" gap={3}>
-        <Button as="a" variant="primary">
-          Button as link
-        </Button>
-        <Button as="a" variant="success">
-          Button as link
-        </Button>
-        <DropDownSearch/>
-      </Stack>
-      <DropdownButton
+      <Form>
+        <Stack direction="horizontal" gap={5}>
+        <InputGroup size="sm" className="mb-1">
+            <InputGroup.Text id="basic-addon1">Trend Analysis</InputGroup.Text>
+            <Form.Control placeholder="Symbol" aria-label="Symbol" 
+              aria-describedby="basic-addon1"
+            />
+            <DatePicker onChange={}/>
+            <Form.Select aria-label="Interval">
+              <option value="ONE_MINUTE">ONE_MINUTE</option>
+              <option value="THREE_MINUTE">THREE_MINUTE</option>
+              <option value="FIVE_MINUTE">FIVE_MINUTE</option>
+              <option value="FIFTEEN_MINUTE">FIFTEEN_MINUTE</option>
+              <option value="THIRTY_MINUTE">THIRTY_MINUTE</option>
+              <option value="ONE_HOUR">ONE_HOUR</option>
+              <option value="DAY">DAY</option>
+              <option value="WEEK">WEEK</option>
+            </Form.Select>
+
+          <Form.Select aria-label="Level">
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+              <option value="40">40</option>
+              <option value="50">50</option>
+              <option value="60">60</option>
+              <option value="70">70</option>
+              <option value="80">80</option>
+              <option value="90">90</option>
+              <option value="95">95</option>
+              <option value="99">99</option>
+            </Form.Select>
+            <Button variant="outline-secondary" id="button-addon1">
+              Send
+            </Button>
+        </InputGroup>          
+          <DropDownSearch/>
+        </Stack>
+      </Form>
+
+      {/* <DropdownButton
       title="Select Series"
       id="dropdown-menu-align-right"
       onSelect={handleSelect}
         >
-              {/* <Dropdown.Item eventKey="Series0">Series 0</Dropdown.Item>
-              
-              <Dropdown.Item eventKey="Series1">Series 1</Dropdown.Item>
-              <Dropdown.Item eventKey="Series2">Series 2</Dropdown.Item>
-              <Dropdown.Item eventKey="Series3">Series 3</Dropdown.Item>
-              <Dropdown.Item eventKey="Series4">Series 4</Dropdown.Item>
-              <Dropdown.Item eventKey="Series5">Series 5</Dropdown.Item>
-              <Dropdown.Item eventKey="Series6">Series 6</Dropdown.Item>
-              <Dropdown.Item eventKey="Series7">Series 7</Dropdown.Item>
-              <Dropdown.Item eventKey="Series8">Series 8</Dropdown.Item>
-              <Dropdown.Item eventKey="Series9">Series 9</Dropdown.Item> */}
               <Dropdown.Item eventKey="Series2Level0">Series 2level 0</Dropdown.Item>
               <Dropdown.Divider />
               <Dropdown.Item eventKey="Series2Level1">Series 2level 1</Dropdown.Item>
@@ -87,16 +109,6 @@ function App() {
               <Dropdown.Item eventKey="Series2Level9">Series 2level 9</Dropdown.Item>
       </DropdownButton>
       <header className="App-header">
-          {/* { value == "Series0" && <CandleStick series={series0} title="Series0"></CandleStick> }
-          { value == "Series1" && <CandleStick series={series1} title="Series1"></CandleStick> }
-          { value == "Series2" && <CandleStick series={series2} title="Series2"></CandleStick> }
-          { value == "Series3" && <CandleStick series={series3} title="Series3"></CandleStick> }
-          { value == "Series4" && <CandleStick series={series4} title="Series4"></CandleStick> }
-          { value == "Series5" && <CandleStick series={series5} title="Series5"></CandleStick> }
-          { value == "Series6" && <CandleStick series={series6} title="Series6"></CandleStick> }
-          { value == "Series7" && <CandleStick series={series7} title="Series7"></CandleStick> }
-          { value == "Series8" && <CandleStick series={series8} title="Series8"></CandleStick> }
-          { value == "Series9" && <CandleStick series={series9} title="Series9"></CandleStick> } */}
           { value == "Series2Level0" && <CandleStick series={series_2level_0} width={[2,2,1]} title="Series 2Level 10%"></CandleStick> }
           { value == "Series2Level1" && <CandleStick series={series_2level_1} width={[2,2,1]} title="Series 2Level 20%"></CandleStick> }
           { value == "Series2Level2" && <CandleStick series={series_2level_2} width={[2,2,1]} title="Series 2Level 30%"></CandleStick> }
@@ -108,12 +120,13 @@ function App() {
           { value == "Series2Level8" && <CandleStick series={series_2level_8} width={[2,2,1]} title="Series 2Level 90%"></CandleStick> }
           { value == "Series2Level9" && <CandleStick series={series_2level_9} width={[2,2,1]} title="Series 2Level 95%"></CandleStick> }
        </header>
-    </div>
-    // <div className="App">
+    </div> */}
+    {/* // <div className="App">
     //   <header className="App-header">
       // <CandleStick series={series}></CandleStick>
     //   </header>
-    // </div>
+    // </div> */}
+    </div>
   );
 }
 
